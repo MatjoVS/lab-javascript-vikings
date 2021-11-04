1384a9ac82c76b6b92f0a1231e61ccb055baae52
Mathieu Von-Seydewitz <CasaDiNoi@Mathieu.local>
2021-11-04 07:18:13 -0400

    battle of the vikings lab

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,66 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health ,strength ) {
+    this.health= health
+    this.strength = strength 
+  }
+  attack(){
+return this.strength;
+  }
+  receiveDamage(damage){
+     this.health=-damage
+  }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor(health, strength, name){
+  super(health, strength)
+  this.name=name
+  }
+  receiveDamage(damage){
+    this.health =- damage;
+    if (this.health > 0){
+      return `${this.name} has received ${damage} points of damage`
+    }
+    else return `${this.name} has died in combat`
+    }
+    battleCry(){
+      return `Odin owns You All!`
+    }
+  }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  receiveDamage(damage){
+    this.health-=damage;
+    if(this.health<= 0 ){`A saxon has died in combat`
+  }else {`A saxon has received ${damage} points of damage`
+
+  }
+
+
+}
 
 // War
-class War {}
+class War {
+  constructor(vikingArmy, SaxonArmy){
+    this.vikingArmy=[]
+    this.saxonArmy=[]
+  }
+  addViking(addOneVikingObj){
+  this.vikingArmy.object.assign(addOneVikingObj)
+  };
+  addSaxon(addOneSaxonObj){
+  this.saxonArmy.object.assign(addOneSaxonObj)
+  };
+  vikingAttack(){
+    let allSaxonArmySoldiers-Math.floor(Math.random()* this.saxonArmy.length)
+  };
+  saxonAttack(){};
+  showStatus(){}
+}
 
 
 
